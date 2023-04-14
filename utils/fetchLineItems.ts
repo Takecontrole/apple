5337fcc68e9c051bac766ff859f38f4e8137547d
Takecontrole <takecontrole2034@gmail.com>
2023-04-14 11:26:19 +0400

    finish

--- utils/fetchLineItems.ts
+++ utils/fetchLineItems.ts
@@ -1,7 +1,7 @@
 export const fetchLineItems = async (sessionId: string) => {
   const res = await fetch(
-    "https://take-control-delta.vercel.app/api/getSession?session_id=${sessionId}"
-  );
+ `http://localhost:3000/api/getSession?session_id=${sessionId}`
+);
 
   if (!res.ok) return;
 
